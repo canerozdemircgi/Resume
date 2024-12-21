910072f2bd6a521b16ae58da81eb5fa610db429c
Caner Özdemir <canerozdemir.mail@gmail.com>
2024-12-21 18:13:55 +0300

    .

--- nodejs/pdf.js
+++ nodejs/pdf.js
@@ -18,7 +18,7 @@ const SavePdf = async (url, output_directory) =>
 	});
 	const page = (await browser.pages())[0];
 	await page.goto(url, {waitUntil: 'networkidle0'});
-	await new Promise(resolve => setTimeout(resolve, 3000));
+	await page.reload();
 	const title = await page.title();
 	await page.emulateMediaType('screen');
 	await page.pdf(
