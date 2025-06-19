e5b2d06c732e405ff6d75018344b10dd0efb5406
Caner Özdemir <canerozdemir.mail@gmail.com>
2025-06-19 21:21:39 +0300

    Added Profile Picture

--- js/index-2.js
+++ js/index-2.js
@@ -13,15 +13,6 @@ if (searchParams.has('outsource'))
 	document.getElementById('span_stats').innerText = '.........';
 }
 
-const layers_left = [...document.querySelectorAll('layer-title-left')];
-const layers_right = [...document.querySelectorAll('layer-title-right')];
-const layer_left_height = layers_left.reduce((sum, element) => sum + element.getBoundingClientRect().height + 40, 0);
-const layer_right_height = layers_right.reduce((sum, element) => sum + element.getBoundingClientRect().height + 40, 0);
-
-const layer_person = document.getElementById('layer_person');
-layer_person.style.maxHeight = `${layer_right_height - layer_left_height - 40}px`;
-layer_person.style.gridTemplateColumns = `minmax(0, ${layer_person.style.maxHeight}) minmax(40px, 1fr) minmax(0, ${layer_person.style.maxHeight}) 10px`;
-
 const layer_about_recess = document.querySelector('#layer_about > text-clause > span');
 const layer_portfolio_recess = document.querySelector('#layer_portfolio > text-clause > span');
 if (layer_about_recess !== null && layer_portfolio_recess !== null)
@@ -48,3 +39,13 @@ else
 		document.getElementById('reminder_version_content_data').textContent = response;
 	});
 }
+
+const layers_left = [...document.querySelectorAll('layer-title-left')];
+const layers_right = [...document.querySelectorAll('layer-title-right')];
+const layer_left_height = layers_left.reduce((sum, element) => sum + element.getBoundingClientRect().height + 40, 0);
+const layer_right_height = layers_right.reduce((sum, element) => sum + element.getBoundingClientRect().height + 40, 0);
+
+const layer_person = document.getElementById('layer_person');
+layer_person.style.maxHeight = `${layer_right_height - layer_left_height - 40}px`;
+layer_person.style.width = '100%';
+layer_person.style.gridTemplateColumns = `minmax(0, ${layer_person.style.maxHeight}) minmax(40px, 1fr) minmax(0, ${layer_person.style.maxHeight}) 10px`;
